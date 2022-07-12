31ed93f7a7b0b64edd98a41e59b57b8af2654865
pauladavid <andreapetro345@gmail.com>
2022-07-11 19:28:15 -0500

    Se agrega proyecto

---
+++ src/HolaMundo.java
@@ -0,0 +1,17 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        String saludar="Hola, como estan. Mi nombre es Paula David";
+        System.out.println(saludar + " Primer proyecto");
+        System.out.println(saludar.toUpperCase());
+        int numero= 10;
+        System.out.println(numero + " es un numero entero");
+        float numeroDecimal= 12.02f;
+        System.out.println(numeroDecimal + " es un numero flotante");
+        double numeroDouble= 12.25;
+        System.out.println(numeroDouble + " es un numero Double");
+        boolean valorT= true;
+        boolean valorF= false;
+        System.out.println(valorT+" y "+valorF+ " son valores Booleanos:");
+
+    }
+}
